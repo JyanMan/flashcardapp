0be2a3bf32e2548a3a5ee980418a75bfe82f5428
JyanMan <196519339+JyanMan@users.noreply.github.com.>
2025-02-20 15:44:21 +0800

    added all the contents of flashcard project, move all files named index to root folder

---
+++ index.js
@@ -0,0 +1,237 @@
+const addBtn = document.getElementById('add-card-btn');
+const saveBtn = document.getElementById('save-card-btn');
+const cardTab = document.querySelector('.card-tab');
+const closeTabBtn = document.getElementById('close-nct-btn');
+const addCardBtn = document.getElementById('add-new-card-btn');
+const newFrontCard = document.querySelector('.new.card-front');
+const newBackCard = document.querySelector('.new.card-back');
+const cardList = document.getElementById('card-list');
+
+const storedCards = sessionStorage.getItem("globalCards");
+const cards = (!storedCards) ? [
+    {id: 0,
+        front: 'frog',
+        back: 'amphibian'
+    },
+    {id: 1,
+        front: 'dog',
+        back: 'mammal'
+    },
+    {id: 2, 
+        front: 'cat',
+        back: 'mammal'
+    },
+    {id: 3,
+        front: 'Watashi',
+        back: `It's me`
+    },
+    {id: 4,
+        front: 'Kore',
+        back: `This`
+    }, 
+    {id: 5,
+        front: 'Yamete',
+        back: `stop`
+    },
+] : JSON.parse(storedCards);
+
+function saveToGlobal() {
+    sessionStorage.setItem("globalCards", JSON.stringify(cards));
+}
+saveToGlobal();
+
+const getNewCard = (id) => {
+    return {
+        id: id,
+        front: newFrontCard.value,
+        back: newBackCard.value
+    };
+}
+
+function cardIsEmpty(front, back) {
+    return front.value === '' || back.value === '';
+}
+
+const addCard = () => {
+    
+    if (newFrontCard.value === '' || newBackCard.value === '') {
+        alert('Please fill out both sides of the card');
+        return;
+    }
+    const newCard = getNewCard(cards.length);
+
+    cards.push(newCard);
+    console.log(cards);
+    renderCards();
+    saveToGlobal();
+}
+
+
+const saveCard = (id) => {
+    const card = cards[id];
+    if (cardIsEmpty(card.front, card.back)) {
+        alert('Please fill out both sides of the card');
+        return;
+    }
+    const newCard = getNewCard(id);
+
+    cards[id] = newCard;
+    console.log(cards);
+    renderCards();
+    //closeCardTab();
+    saveToGlobal();
+}
+
+const removeCard = (id) => {
+    console.log(cards);
+    console.log("removed");
+    if (cards.length === 1) {
+        cards.pop();
+    }
+    console.log("BEFORE", cards);
+    cards.splice(id, 1);
+    renderCards();
+    saveToGlobal();
+    console.log("AFTER", cards);
+    //rearrange card id
+    for (let i = 0; i <= cards.length-1; i++) {
+        cards[i].id = i;
+    }
+    openAddCardTab();
+}
+
+const renderCards = () => {
+    //cardList.innerHTML = '';
+
+    while (cardList.firstChild) {
+        cardList.removeChild(cardList.firstChild);
+    }
+
+    if (cards) {
+
+        cards.forEach(card => {
+            const cardElement = document.createElement('li');
+            cardElement.className = 'card';
+            const cardContent = document.createTextNode(card.front + ' - ' + card.back);
+            cardElement.appendChild(cardContent);
+    
+            const deleteBtn = document.createElement('button');
+            deleteBtn.className = 'delete-btn';
+            deleteBtn.onclick = () => removeCard(card.id);
+    
+            cardElement.addEventListener('click', () => {openEditCardTab(card)});
+    
+            cardElement.appendChild(deleteBtn);
+            cardList.appendChild(cardElement);
+        })
+    }
+}
+
+const closeCardTab = () => {
+    cardTab.style.display = 'none';
+}
+
+let saveHandler;
+const openEditCardTab = (card) => {
+    //console.log("opned");
+    if (cards.includes(card) === false) {
+        return;
+    }
+    console.log("openedcard tab");
+    //console.log(cards);
+    cardTab.style.display = 'flex';
+    newBackCard.value = card.back;
+    newFrontCard.value = card.front;
+    addCardBtn.style.display = 'none';
+    saveBtn.style.display = 'block';
+
+    if (saveHandler) {
+        saveBtn.removeEventListener('click', saveHandler);
+    }
+    saveHandler = () => saveCard(card.id);
+    saveBtn.addEventListener('click', saveHandler);
+
+}
+
+const openAddCardTab = () =>  {
+    cardTab.style.display = 'flex';
+    newFrontCard.value = '';
+    newBackCard.value = '';
+    addCardBtn.style.display = 'block';
+    saveBtn.style.display = 'none';
+}
+
+if (addCardBtn) {
+
+    addCardBtn.addEventListener('click', addCard);
+    addBtn.addEventListener('click', openAddCardTab);
+    closeTabBtn.addEventListener('click', closeCardTab);
+    
+    renderCards();
+}
+
+//add draggability of card tab
+function dragElement() {
+    const moveArea = document.getElementById("move-card-tab");
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    const dragMouseDown = (e) => {
+        if (cardTab.style.position !== 'absolute') {
+            cardTab.style.position = 'absolute';
+        }
+        e = e || window.event;
+        e.preventDefault();
+
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+    }
+
+    const elementDrag = (e) => {
+        e = e || window.event;
+        e.preventDefault();
+
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+
+        cardTab.style.top = (cardTab.offsetTop - pos2) + "px";
+        cardTab.style.left = (cardTab.offsetLeft - pos1) + "px";
+        
+        if (elementOverElement(moveArea, cardTab.parentElement)) {
+            cardTab.style.opacity = '0.5';
+        }
+        else {
+            cardTab.style.opacity = '1';
+        }
+    }
+    const closeDragElement = () => {
+        document.onmouseup = null;
+        document.onmousemove = null;
+        if (elementOverElement(moveArea, cardTab.parentElement)) {
+            cardTab.style.position = 'static';
+            cardTab.style.left = '0px';
+            cardTab.style.top = '0px';
+            cardTab.style.opacity = '1';
+        }
+    }
+    if (moveArea) {
+        moveArea.onmousedown = dragMouseDown;
+    }
+}
+
+function elementOverElement(el1, el2) {
+    const topRect = el1.getBoundingClientRect();
+    const bottomRect = el2.getBoundingClientRect();
+
+    return (
+        topRect.left >= bottomRect.left &&
+        topRect.right <= bottomRect.right &&
+        topRect.top >= bottomRect.top &&
+        topRect.bottom <= bottomRect.bottom
+    );
+}
+
+dragElement();
---
+++ scripts/review.js
@@ -0,0 +1,237 @@
+//import {cards} from '../scripts/index.js';
+const storedCards = sessionStorage.getItem("globalCards");
+const cardsToReview = storedCards ? JSON.parse(storedCards) : [];
+console.log(cardsToReview);
+
+const nextBtn = document.getElementById('next-btn');
+//const cardsToReview = [...cards];
+
+let currentCardIndex = 0;
+let flipped = false;
+
+const maxDisplayedCards = 20;
+let cardWidth = 200;
+let cardHeight = 200;
+
+const toDarkColor = '#629161';
+const toLightColor = `#ccc9bf`;
+
+//LEARNED SIMPLE BACKEND LEZZ GOOO!!!
+
+const flipCard = (id) => {
+    flipped = true;
+    const cardBack = document.querySelector('.first.card').querySelector('.card-back');
+    cardBack.style.display = (cardBack.style.display === 'none') ? 'block' : 'none';
+}
+
+const renderCards = (front, back, id) => {
+    flipped = false;
+    currentCardIndex = id;
+    //console.log("rendercardID", currentCardIndex);
+    const reviewCardContainer = document.getElementById('card-to-review');
+    const cardElement = document.createElement('div');
+
+    //setup the first card (the card that is being reviewed)
+    cardElement.setAttribute('class', 'first card');
+    cardElement.id = id;
+    cardElement.style.zIndex = 21;
+    cardElement.addEventListener('click', () => flipCard(id));
+    cardElement.innerHTML = `
+        <div class="card-front">${front}</div>
+        <div class="card-back" style="display: none;">${back}</div>`;
+    //replace the first card if it already exists
+    // otherwise add the first card
+    if (reviewCardContainer.firstChild) {
+        reviewCardContainer.replaceChild(cardElement, reviewCardContainer.firstChild);
+    }
+    else {
+        reviewCardContainer.appendChild(cardElement);
+    }
+    if (cardsToReview.length === 1) {
+        console.log("canceled");
+        return;
+    }
+
+    renderDisplayCards();
+}
+
+const renderDisplayCards = () => {
+    const cardContainer = document.getElementById("card-stack");
+    while (cardContainer.firstChild) {
+        cardContainer.removeChild(cardContainer.firstChild);
+    }
+    //calculate how many extra cards to display
+    let displayedExtraCards = 0;
+    if (cardsToReview.length > maxDisplayedCards) {
+        displayedExtraCards = maxDisplayedCards-1;
+    }
+    else {
+        displayedExtraCards = cardsToReview.length-1;
+    }
+
+    cardWidth = document.querySelector('.card').getBoundingClientRect().width;
+    cardHeight = document.querySelector('.card').getBoundingClientRect().height;
+    for (let i = 0; i <= displayedExtraCards-1; i++) {
+        const displayCard = document.createElement('div');
+
+        if (i === 0) displayCard.setAttribute('class', `first display card`);
+        else displayCard.setAttribute('class', `display card`);
+        //displayCard.innerText = 'dCard ' + i;
+        displayCard.style.width = `${cardWidth}px`;
+        displayCard.style.height =  `${cardHeight}px`;
+
+        //get the 100%
+        //subtract that by the width
+        const widthFromEnd = `(${100}% - ${cardWidth}px)`;
+        //you get that difference and divide it by the amount of cards
+        //the result is the amount of spaces for each card
+        const cardLeft = `(${widthFromEnd} / ${displayedExtraCards-1})`;
+        
+        displayCard.style.left = `calc(${i} * ${cardLeft})`;
+        displayCard.style.zIndex = `${maxDisplayedCards-i}`;
+        if (i === 0) {
+            displayCard.style.zIndex = maxDisplayedCards;
+        }
+
+        cardContainer.appendChild(displayCard);
+    }
+}
+
+window.addEventListener("resize", () => 
+    renderDisplayCards());
+
+let animated = false;
+async function nextCard() {
+    animated = false;
+    console.log(currentCardIndex);
+    let currentCard = cardsToReview[currentCardIndex];
+
+    if (flipped === false) {
+        flipCard(currentCard.id);
+        return;
+    }
+
+    //change card 1 on display to the next review card
+    
+    
+    if (!animated) {
+        if (currentCardIndex === cardsToReview.length-1) {
+            currentCardIndex = 0;
+        }
+        else {
+            currentCardIndex++;
+        }
+        cardBehindToNextCard(currentCardIndex);
+        currentCard = cardsToReview[currentCardIndex];
+        await animate();
+        renderCards(currentCard.front, currentCard.back, currentCard.id);
+    }
+}
+
+const cardBehindToNextCard = (cardIndex) => {
+    //get the currentcard
+    //get the card behind (card on display)
+    const cardBehind = document.querySelector('.first.display.card');
+
+    //get the fron of the next card
+    const frontOfCard = cardsToReview[cardIndex].front;
+    //set the inner text of card behind to the front of nextCard
+    cardBehind.innerText = frontOfCard;
+    cardBehind.style.backgroundColor = toLightColor;
+    //to the front and back of the next card
+}
+
+const getRightOfLastCard = () => {
+    const displayCards =  document.querySelectorAll('.display.card');
+    let rightOfLastCard;
+    if (displayCards.length > 2) {
+
+        rightOfLastCard = displayCards[displayCards.length-2].getBoundingClientRect().right;
+    }
+    else {
+        rightOfLastCard = displayCards[displayCards.length-1].getBoundingClientRect().right;
+
+    }
+    return window.innerWidth - rightOfLastCard;
+}
+
+const animate = () => {
+    //rcPos is reviewCardPosition
+    const increment = 15;
+    const displayCardPos = 100/cardsToReview.length;
+    const interval = displayCardPos/increment;
+    
+    const reviewCard = document.querySelector('.first.card');
+    const reviewCardWidth = reviewCard.getBoundingClientRect().width;
+    const firstDisplayCard = document.querySelector('.first.display.card');
+    const displayCard = document.querySelectorAll('.display.card');
+    const reviewCardLastLeft = reviewCard.getBoundingClientRect().left;
+    const displayCardLastLeft = firstDisplayCard.getBoundingClientRect().left;
+    const reviewToDisplayDist = displayCardLastLeft-reviewCardLastLeft;
+
+    const winWidth = window.innerWidth;
+    const rightOfLastCard = getRightOfLastCard();
+    const theIncrement = (winWidth-reviewCardWidth)/increment;
+    return new Promise((resolve) => {
+
+        //move the first card to the end
+        let pos1 = reviewCardLastLeft;
+        const goBackWidth = winWidth-rightOfLastCard;
+        let pos2 = pos1;
+        function step() {
+            pos2 = pos1
+            if (pos1 < winWidth-(reviewCardWidth*(-0.5))) {
+                //console.log(pos1);
+                const reviewCardCurrentLeft = reviewCard.getBoundingClientRect().left;
+                const displayCardCurrentLeft = firstDisplayCard.getBoundingClientRect().left-displayCardLastLeft;
+                //console.log(reviewCardCurrentLeft, pos1);
+                if (pos1 >= goBackWidth) {
+                    reviewCard.style.left = `calc(${reviewCardCurrentLeft-(theIncrement*0.5)}px)`;
+                }
+                else reviewCard.style.left = `calc(${reviewCardCurrentLeft+theIncrement}px)`;
+                pos1 += theIncrement;
+                //console.log(pos1-pos2);
+
+                if (pos1 >= goBackWidth *0.8) {
+                    reviewCard.style.zIndex = 1;
+                    reviewCard.style.backgroundColor = toDarkColor;
+                }
+                if (firstDisplayCard.getBoundingClientRect().left > reviewCardLastLeft)
+                    firstDisplayCard.style.left = `${displayCardCurrentLeft - (reviewToDisplayDist/increment*0.8)}px`;
+                
+                //this prevents the display cards to go out of the card stack
+                if (displayCard.length > 1) {
+                    //console.log(displayCard.length, displayCard)
+                    if (displayCard[1].getBoundingClientRect().left > displayCardLastLeft) {
+                        displayCard.forEach((card) => {
+                            if (card !== firstDisplayCard) {
+                                
+                                card.style.left = `calc(${card.style.left} - ${interval}%)`;
+                            }
+                            //console.log(card.innerText, card.style.left);
+                        });
+                    }
+                }
+                requestAnimationFrame(step);
+            }
+            else {
+                animated = true;
+                resolve();
+            }
+        }
+        requestAnimationFrame(step);
+    })
+}
+
+if (cardsToReview && cardsToReview.length > 0) {
+
+    renderCards(cardsToReview[0].front, cardsToReview[0].back, cardsToReview[0].id);
+    nextBtn.addEventListener('click', nextCard);
+}
+else {
+    alert("You have no cards to review");
+    nextBtn.addEventListener('click', function() {
+        alert("Add cards to your deck to have cards to review");
+    });
+}
+
